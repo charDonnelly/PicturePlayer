ae293769e637f455ddeb83b6cb26d00fab31ecf5
Thomas Woodside <thomas.woodside@gmail.com>
2016-06-15 13:47:14 -0700

    Updated filepaths.

--- Looma-PicturePlayer.php
+++ Looma-PicturePlayer.php
@@ -14,8 +14,8 @@ Comments:
 -->
 
 <?php $page_title = 'Looma - put page title here';
-include ('includes/header.php');
-/*OPTIONAL*/ include ('includes/mongo-connect.php');
+include ('../Looma-II/includes/header.php');
+/*OPTIONAL*/ include ('../Looma-II/includes/mongo-connect.php');
 ?>
 <head>
     <meta charset="utf-8" />
@@ -23,32 +23,32 @@ include ('includes/header.php');
     <meta name="author"  content="Skip">
     <meta name="project" content="Looma">
     <meta name="owner"   content="villagetechsolutions.org">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../Looma-II/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../Looma-II/images/favicon-96x96.png">
     <meta http-equiv="X-UA-Compatible" content="IE-edge"> <!-- uses latest IE rendering engine-->
-    <!--[if lt IE 9]> <script src="js/html5shiv.min.js"></script>  <![endif]-->
+    <!--[if lt IE 9]> <script src="../Looma-II/js/html5shiv.min.js"></script>  <![endif]-->
 
 
     <title> Looma - - page title here </title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">     <!-- Bootstrap CSS still needed ?? [but dont neet the JS]-->
-    <link rel="stylesheet" href="css/looma.css">        	 <!-- Looma CSS -->
-    <link rel="stylesheet" href="css/looma-theme-looma.css">        	 <!-- Looma CSS -->
+    <link rel="stylesheet" href="../Looma-II/css/bootstrap.min.css">     <!-- Bootstrap CSS still needed ?? [but dont neet the JS]-->
+    <link rel="stylesheet" href="../Looma-II/css/looma.css">        	 <!-- Looma CSS -->
+    <link rel="stylesheet" href="../Looma-II/css/looma-theme-looma.css">        	 <!-- Looma CSS -->
 </head>
 
 <body>
 <div id="main-container-horizontal">
 
-    <img src="images/logos/LoomaLogoTransparent.png" class="looma-logo" width="75%"/>
+    <img src="../Looma-II/images/logos/LoomaLogoTransparent.png" class="looma-logo" width="75%"/>
     <h1>This is looma-template.php</h1>
 
 </div>
 
 <?php
 /*include either, but not both, of toolbar.php or toolbar-vertical.php*/
-include ('includes/toolbar.php');
+include ('../Looma-II/includes/toolbar.php');
 /*include ('includes/toolbar-vertical.php'); */
-include ('includes/js-includes.php');
+include ('../Looma-II/includes/js-includes.php');
 ?>
 
 
@@ -59,7 +59,7 @@ include ('includes/js-includes.php');
 
 
 
-<script src="js/jquery.js">          </script>      <!-- jQuery -->
-<script src="js/looma-utilities.js"> </script>      <!-- Looma utility functions -->
-<script src="js/looma.js">           </script>      <!-- Looma common page functions -->
+<script src="../Looma-II/js/jquery.js">          </script>      <!-- jQuery -->
+<script src="../Looma-II/js/looma-utilities.js"> </script>      <!-- Looma utility functions -->
+<script src="../Looma-II/js/looma.js">           </script>      <!-- Looma common page functions -->
 </body>
